8c5aa3d18964d5daf262ac94a4b61e836cdb2f48
Fabian Wagner <fabianwagner.fwag@gmail.com>
2026-06-16 03:21:52 +0200

    route für gruppen erstellt in app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,21 @@ import './App.scss';
 import Hero from './components/Hero/Hero';
 import ScrollingFlags from './components/ScrollingFlags/ScrollingFlags';
 import Groups from './components/Groups/Groups';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <Hero />
       <ScrollingFlags />
-      <Groups />
+      <Router>
+        <Routes>
+          <Route 
+            path="/"
+            element={<Groups />}
+          />
+        </Routes>
+      </Router>
     </div>
   )
 }
